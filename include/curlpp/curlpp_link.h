868b313356007be41c60870d1c04cb9f7a6defe9
debugee <519306795@qq.com>
2023-07-25 20:03:23 +0800

    link.h

--- include/curlpp/curlpp_link.h
+++ include/curlpp/curlpp_link.h
@@ -2,7 +2,31 @@
 
 
 #ifdef _DEBUG
-#pragma comment(lib, "v12_smdd_curlpp.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_curlpp.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_curlpp.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_curlpp.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_curlpp.lib")
+		#endif
+	#endif
 #else
-#pragma comment(lib, "v12_smd_curlpp.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_curlpp.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_curlpp.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_curlpp.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_curlpp.lib")
+		#endif
+	#endif
 #endif

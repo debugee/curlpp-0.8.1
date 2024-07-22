bb19c7deda9677104a70678af7d862959814cc8a
debugee <519306795@qq.com>
2024-07-22 20:14:06 +0800

    MSVC_TOOLSET_VERSION

--- include/curlpp/curlpp_link.h
+++ include/curlpp/curlpp_link.h
@@ -3,29 +3,29 @@
 
 #ifdef _DEBUG
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smdd_curlpp.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smdd_curlpp.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smtd_curlpp.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smtd_curlpp.lib")
 		#endif
 	#endif
 #else
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smd_curlpp.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smd_curlpp.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smt_curlpp.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smt_curlpp.lib")
 		#endif
 	#endif
